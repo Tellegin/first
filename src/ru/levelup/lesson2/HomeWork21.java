727bd167a412159876e0223a51656b4d53d79ce3
tellegin <teleginrus@gmail.com>
2022-05-22 21:21:48 +0300

    3

--- src/ru/levelup/lesson2/HomeWork21.java
+++ src/ru/levelup/lesson2/HomeWork21.java
@@ -1,7 +1,18 @@
 package ru.levelup.lesson2;
+import java.util.Scanner;
 
 public class HomeWork21 {
     public static void main(String[] args) {
+        System.out.println("Введите число. Программа определит четное оно или нечетное:");
+        Scanner op = new Scanner(System.in);
+        double a = op.nextDouble();
+        double b = a%2;
+        if (b > 0) {
+
+            System.out.println("Вы ввели нечетное число");
+        } else {
+            System.out.println("Вы ввели четное число");
+        }
 
     }
 }
